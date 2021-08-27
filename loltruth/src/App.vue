b9a839db40bae9907da34a9f3ba297565185f48c
come-collab <cometrisse640@gmail.com>
2021-08-27 11:04:34 +0200

    Understanding a little bit better vue.js and organizing the project

--- loltruth/src/App.vue
+++ loltruth/src/App.vue
@@ -1,49 +1,23 @@
 <template>
   <div id="app">
-    <h1> League of legend truth based on data</h1>
-    <p>This website have for goal to explain to people why League of Legend is top notch in terms of competition
-    And why the dream of becoming a pro player/high rank player is extremely difficult to reach
-    The whole website fetching of data, process of treating those data's and code is available on <a href = "https://github.com/come-collab">GitHub </a> 
-    </p>
-    <button onclick="toggleText()"><strong>Tech Sidenote :</strong></button>
-    <p id="demo" style="display : none"> 
-    This site comes with a scrapper. The data is taken from multiples sources League of Legend API,
-    and op.gg website. The study, the ideas, the data visualisation is done by me and is available on github , most of the intelligence of
-    the app is on datascrapper.py file. If you want to learn more about that feel free to check it out but even if you are not into Tech
-    this website is for you 
-    </p>
-    <p><strong>Idea explanation :</strong>The whole point is to help people to have more realistic goals on LoL and help them enjoy the game more without being devoured by the 
-      unrealistic need to be a high elo player. This website is going to show what the majority of high elo player share in commun and why they
-      are at the place they are at and that a big part of that is inner skill.
-      The idea came from a video of the very famous LoL Coach LS who said that every pro player he ever encountered reach high elo in less than two years of playing the game
-      thats why i lead this study, to help people understand that on such a hard game reaching top level is not for everybody.
-      in this first graph you will see that one of those thing is the first rank they ever reach. Of course all the accounts selected are mains and not smurf.
-      Enjoy !
-    </p>
+    <MainText/>
     <PlanetChart/>
   </div>
 </template>
 
 <script>
 import PlanetChart from './components/PlanetChart.vue'
+import MainText from './components/MainText.vue'
 
 export default {
   name: 'App',
   components: {
-    PlanetChart
+    PlanetChart,
+    MainText
   }
 }
 </script>
-<script>
-export function toggleText() {
-  var text = document.getElementById("demo");
-  if (text.style.display === "none") {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
-}
-</script>
+
 
 
 
