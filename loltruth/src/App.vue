08ad624b6443a26e1e001261201037b2fb6418ef
come-collab <cometrisse640@gmail.com>
2021-08-27 14:01:51 +0200

    front end stuff done

--- loltruth/src/App.vue
+++ loltruth/src/App.vue
@@ -33,5 +33,5 @@ export default {
   margin-left : 15%;
 }
 p { font-family: Quicksand; font-size: 14px; font-style: normal; font-variant: normal; font-weight: 400; line-height: 20px; }
-h1 { font-family: TimesNewRoman, "Times New Roman", Times, Baskerville, Georgia, serif; font-size: 24px; font-style: normal; font-variant: normal; font-weight: 700; line-height: 26.4px; } 
+h1 { font-family: Quicksand; font-size: 24px; font-style: normal; font-variant: normal; font-weight: 700; line-height: 26.4px; }
 </style>
